181c7d1b5201f9286e93b19a38ff901625bc9edc
Nick Lauder <nick@quantumsecurity.co.nz>
2026-05-27 11:43:32 +1200

    Remvoed version from output filename

--- test/TestRunner.ts
+++ test/TestRunner.ts
@@ -87,7 +87,7 @@ async function runTestIteration(useOfflinePackage: boolean): Promise<void> {
 				"--install-extension",
 				Path.join(
 					ltexDirPath,
-					`vscode-ltex-bre-${ltexVersion}-offline-${platform}-${architecture}.vsix`,
+					`vscode-ltex-bre-offline-${platform}-${architecture}.vsix`,
 				),
 			);
 		}
